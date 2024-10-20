eb0ab8d9b2312732504aebc3fb546cb9171d0670
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2024-10-20 05:46:17 +0530

    updating image

--- airbnb/src/Components/Card.jsx
+++ airbnb/src/Components/Card.jsx
@@ -5,7 +5,7 @@ const card = () => {
     <>
     <div className="card h-[360px] m-4 flex flex-col justify-between">
         <div className="image">
-            <img src="https://random.imagecdn.app/500/150" alt="Homestay image" className='h-[270px] w-[300px] object-cover rounded-2xl'/>
+            <img src="https://img.freepik.com/free-photo/photorealistic-house-with-wooden-architecture-timber-structure_23-2151302742.jpg?semt=ais_hybrid" alt="Homestay image" className='h-[270px] w-[300px] object-cover rounded-2xl'/>
         </div>
         <div>
             <h5 className='font-semibold text-sm'>Mashobra, India</h5>
